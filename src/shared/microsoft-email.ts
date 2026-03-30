2b69993bea9ed28987d0ffd6a5ba06f6761074ff
CoWork OS <hello@cowork-os.com>
2026-03-30 10:18:15 +0100

    Define Microsoft email OAuth constants

---
+++ src/shared/microsoft-email.ts
@@ -0,0 +1,8 @@
+export const MICROSOFT_EMAIL_OAUTH_DEFAULT_SCOPES = [
+  "https://outlook.office.com/IMAP.AccessAsUser.All",
+  "https://outlook.office.com/SMTP.Send",
+  "offline_access",
+] as const;
+
+export const MICROSOFT_EMAIL_DEFAULT_TENANT = "consumers";
+export const MICROSOFT_EMAIL_OAUTH_PROVIDER = "microsoft";
