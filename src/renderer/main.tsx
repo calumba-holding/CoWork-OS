e8d1064e5ec0230554e7d8c23aecf9f4423ce574
CoWork OS <hello@cowork-os.com>
2026-05-17 16:32:46 +0100

    ui: update renderer app

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./react-refresh-ignored-exports";
 import { App } from "./App";
 import "./i18n"; // Initialize i18n before rendering
 import "./styles/index.css";
